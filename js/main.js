a9bc43eb72a6b88ad5d300f9c83baf904668d862
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-06-30 01:15:05 +0200

    Fix Js Imgs

--- js/main.js
+++ js/main.js
@@ -109,10 +109,10 @@ bgBtn.forEach((item) => {
 let landingPage = document.querySelector(".landing");
 
 index = 0;
-img1 = `url(/images/1.jpg)`;
-img2 = `url(/images/2.jpg)`;
-img3 = `url(/images/3.jpg)`;
-img4 = `url(/images/4.jpg)`;
+img1 = `url(images/1.jpg)`;
+img2 = `url(images/2.jpg)`;
+img3 = `url(images/3.jpg)`;
+img4 = `url(images/4.jpg)`;
 let arrImages = [img1, img2, img3, img4];
 
 function selectImage() {
